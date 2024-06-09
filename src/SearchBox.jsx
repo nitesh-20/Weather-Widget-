9e9d2137fdd0ecf95badbdc3df45435306a8b12f
nitesh-20 <nsahu6156@gmail.com>
2024-06-09 19:59:29 +0530

    Api add and check

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,6 +6,25 @@ import { useState } from 'react';
 export default function SearchBox() {
     let [city, setCity] = useState('');
 
+    const API_URL ="https://api.openweathermap.org/data/2.5/weather"
+    const API_KEY = "94b836da3a697c9d296e405605713864"
+
+    let getWeatherInfo = async  () =>{
+          let response =  await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`) 
+        let jsonResponse =  await response.json()
+        let result = {
+             temp:jsonResponse.main.temp,
+             tempMin:jsonResponse.main.temp_min,
+             tempMax:jsonResponse.main.temp_max,
+             humidity:jsonResponse.main.humidity,
+             feelsLike:jsonResponse.main.feels_like,         
+             weather:jsonResponse.weather[0].description,
+        }
+        console.log(result);
+    }
+
+    
+
     let handleChange = (evt) => {
         setCity(evt.target.value);
     }
@@ -13,6 +32,7 @@ export default function SearchBox() {
         evt.preventDefault();
         console.log(city);
         setCity("")
+        getWeatherInfo()
     }
 
     return (
